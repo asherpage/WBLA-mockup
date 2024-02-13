8c7f6744b5bbd5d91aa99fbe6de20967a015ac64
asherpage <110576723+asherpage@users.noreply.github.com>
2024-02-12 21:56:15 -0700

    navbar changes

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ window.addEventListener('scroll', function() {
     const parallax = document.querySelector('.background');
     let scrollPosition = window.pageYOffset;
     parallax.style.backgroundPositionY = scrollPosition * 0.7 + 'px'; // Adjust the speed of parallax
-    const navbar = document.querySelector('.navbar');
     const bannerHeight = document.querySelector('.background').offsetHeight;
-    const navbarLinks = document.querySelectorAll('.links a');
+    const others = document.querySelectorAll('.others');
+        const homer = document.querySelectorAll('#homer');
     
     if (scrollPosition > bannerHeight) {
-        navbarLinks.forEach(link => link.style.color = 'black');
+        others.forEach(link => link.style.color = 'black');
     } else {
-        navbarLinks.forEach(link => link.style.color = 'white');
+        others.forEach(link => link.style.color = 'white');
     }
 });
