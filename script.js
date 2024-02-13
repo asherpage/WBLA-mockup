9edec74334e915ff6e0e93b0028321615c156559
asherpage <110576723+asherpage@users.noreply.github.com>
2024-02-12 22:59:12 -0700

    few more elements

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ window.addEventListener('scroll', function() {
     parallax.style.backgroundPositionY = scrollPosition * 0.7 + 'px'; // Adjust the speed of parallax
     const bannerHeight = document.querySelector('.background').offsetHeight;
     const others = document.querySelectorAll('.others');
-        const homer = document.querySelectorAll('#homer');
+    const navbar = document.querySelector('.navbar');
     
     if (scrollPosition > bannerHeight) {
         others.forEach(link => link.style.color = 'black');
+        navbar.classList.add('white-bg');
     } else {
         others.forEach(link => link.style.color = 'white');
+        navbar.classList.remove('white-bg');
     }
 });
